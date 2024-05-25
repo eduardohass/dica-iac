0b853fd46ac42b297dc4e15cda511b5db2ea6e1e
eduardohass <eduardohass@outlook.com>
2024-05-25 16:52:11 -0300

    fix names

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "dica_database" {
   allocated_storage   = 20
   engine              = "postgres"
   engine_version      = "16.1"
-  identifier          = "dica_postgres_db"
+  identifier          = "dicadb"
   instance_class      = "db.t3.micro"
   username            = "dica_admin"
   password            = "DicaAdmin321!#"
