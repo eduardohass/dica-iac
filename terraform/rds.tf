a9a26e5b64c40d21a8ffe0d1ebb3f4c4fd31708d
eduardohass <eduardohass@outlook.com>
2024-05-25 16:36:18 -0300

    fix version

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,15 +1,15 @@
 resource "aws_db_instance" "dica_database" {
-  allocated_storage    = 20
-  storage_type         = "gp2"
-  engine               = "postgres"
-  engine_version       = "13.3"
-  instance_class       = "db.t2.micro" # Free Tier eligible
-  db_name              = "dica"
-  username             = "dica_admin"
-  password             = "DicaAdmin32!#"
-  parameter_group_name = "default.postgres13"
-  skip_final_snapshot  = true
-  publicly_accessible  = false
+  allocated_storage   = 20
+  engine              = "postgres"
+  engine_version      = "11.5"
+  identifier          = "dica-postgres-db"
+  instance_class      = "db.t2.micro"
+  username            = "dica-admin"
+  password            = "DicaAdmin321!#"
+  skip_final_snapshot = true
+  storage_encrypted   = false
+  publicly_accessible = true
+  apply_immediately   = true
   tags = {
     "managedby" = "terraform"
   }
