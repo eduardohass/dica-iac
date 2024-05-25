fca61f8ec28b3283a5bd7139cef862e1e7d3dfba
eduardohass <eduardohass@outlook.com>
2024-05-25 16:40:32 -0300

    fix version

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -1,9 +1,9 @@
 resource "aws_db_instance" "dica_database" {
   allocated_storage   = 20
   engine              = "postgres"
-  engine_version      = "11.5"
+  engine_version      = "16.3-R1"
   identifier          = "dica-postgres-db"
-  instance_class      = "db.t2.micro"
+  instance_class      = "db.t3.micro"
   username            = "dica-admin"
   password            = "DicaAdmin321!#"
   skip_final_snapshot = true
