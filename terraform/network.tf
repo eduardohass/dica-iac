dc2c6bb9f27969bf497b89d6dd4a7c038952d6d6
eduardohass <eduardohass@outlook.com>
2024-05-26 19:21:57 -0300

    feat: make subnet public

--- terraform/network.tf
+++ terraform/network.tf
@@ -12,6 +12,7 @@ resource "aws_subnet" "sub-a" {
   cidr_block        = "10.0.1.0/24"
   vpc_id            = aws_vpc.dica-vpc.id
   availability_zone = var.aws_availability_zone_a
+  map_public_ip_on_launch = true
   tags = {
     "Name" = "sub-a",
     "managedby" = "terraform"
@@ -32,6 +33,7 @@ resource "aws_subnet" "sub-c" {
   cidr_block        = "10.0.3.0/24"
   vpc_id            = aws_vpc.dica-vpc.id
   availability_zone = var.aws_availability_zone_b
+  map_public_ip_on_launch = true
   tags = {
     "Name" = "sub-c",
     "managedby" = "terraform"
