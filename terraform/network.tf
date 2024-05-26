a20800ec517e35fbf7da8d67476b3abadab5d415
eduardohass <eduardohass@outlook.com>
2024-05-26 17:31:24 -0300

    feat: add new tag

--- terraform/network.tf
+++ terraform/network.tf
@@ -3,7 +3,8 @@ resource "aws_vpc" "dica-vpc" {
   enable_dns_hostnames = true
   enable_dns_support   = true
   tags = {
-    "Name" = "dica-vpc"
+    "Name" = "dica-vpc",
+    "managedby" = "terraform"
   }
 }
 
