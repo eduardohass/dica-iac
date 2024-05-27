2fdc28fd3177c2db6d4f0f65c67664c6bbab45a4
eduardohass <eduardohass@outlook.com>
2024-05-26 21:42:27 -0300

    feat: add rds pg

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -11,8 +11,19 @@ resource "aws_db_instance" "dica_database" {
   publicly_accessible    = true
   apply_immediately      = true
   vpc_security_group_ids = [aws_security_group.ingress-rds-pg.id]
+  db_subnet_group_name   = aws_db_subnet_group.dica_rds_subnet_group.name
   tags = {
     "Name"      = "dica"
+    "managedby" = "terraform"
+  }
+}
+
+resource "aws_db_subnet_group" "dica_rds_subnet_group" {
+  name       = "my-db-subnet-group"
+  subnet_ids = [aws_subnet.sub-a.id, aws_subnet.sub-c.id]
+
+  tags = {
+    Name        = "Dica RDS Postgres Subnet Group"
     "managedby" = "terraform"
   }
 }
